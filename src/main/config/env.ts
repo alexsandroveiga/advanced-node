68b1d61b278342999b53404b1af45f83ba2ea761
Alexsandro <sandro.alex_@hotmail.com>
2021-07-28 21:56:17 -0300

    add Facebook Login factories

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,6 @@ export const env = {
     cliendId: process.env.FB_CLIENT_ID ?? '253238079698622',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '92bb457d6b57ac72978de376cd47fe9e'
   },
-  port: process.env.PORT ?? 8080
+  port: process.env.PORT ?? 8080,
+  jwtSecret: process.env.JWT_SECRET ?? 'as4as84as14'
 }
---
+++ src/main/factories/apis/facebook.ts
@@ -0,0 +1,10 @@
+import { FacebookApi } from '@/infra/apis'
+import { env } from '@/main/config/env'
+import { makeAxiosHttpClient } from '@/main/factories/http'
+
+export const makeFacebookApi = (): FacebookApi => {
+  return new FacebookApi(
+    makeAxiosHttpClient(),
+    env.facebookApi.cliendId,
+    env.facebookApi.clientSecret)
+}
