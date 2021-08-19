f6168bd7c11f74c47264416c02e3467243ca4c3d
Alexsandro <sandro.alex_@hotmail.com>
2021-08-18 23:06:17 -0300

    improve architecture

--- tests/infra/repos/postgres/user-account.spec.ts
+++ tests/infra/repos/postgres/user-account.spec.ts
@@ -1,6 +1,6 @@
-import { PgUserAccountRepository } from '@/infra/postgres/repos'
-import { PgUser } from '@/infra/postgres/entities'
-import { makeFakeDb } from '@/tests/infra/postgres/mocks'
+import { PgUserAccountRepository } from '@/infra/repos/postgres'
+import { PgUser } from '@/infra/repos/postgres/entities'
+import { makeFakeDb } from '@/tests/infra/repos/postgres/mocks'
 
 import { getRepository, Repository, getConnection } from 'typeorm'
 import { IBackup } from 'pg-mem'
--- tests/main/routes/login.spec.ts
+++ tests/main/routes/login.spec.ts
@@ -1,7 +1,7 @@
 import { app } from '@/main/config/app'
 import { UnauthorizedError } from '@/application/errors'
-import { makeFakeDb } from '@/tests/infra/postgres/mocks'
-import { PgUser } from '@/infra/postgres/entities'
+import { makeFakeDb } from '@/tests/infra/repos/postgres/mocks'
+import { PgUser } from '@/infra/repos/postgres/entities'
 
 import { IBackup } from 'pg-mem'
 import request from 'supertest'
@@ -12,7 +12,7 @@ describe('Login Routes', () => {
     let backup: IBackup
     const loadUserSpy = jest.fn()
 
-    jest.mock('@/infra/apis/facebook', () => ({
+    jest.mock('@/infra/gateways/facebook-api', () => ({
       FacebookApi: jest.fn().mockReturnValue({ loadUser: loadUserSpy })
     }))
 
